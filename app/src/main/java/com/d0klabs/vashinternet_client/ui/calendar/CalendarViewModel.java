6ce0a2ba6ed8bd5572f194b650ba105af7feb376
D0kL0d <devtemp411@bigmir.net>
2022-01-16 14:22:49 +0200

    motion creation

---
+++ app/src/main/java/com/d0klabs/vashinternet_client/ui/calendar/CalendarViewModel.java
@@ -0,0 +1,11 @@
+package com.d0klabs.vashinternet_client.ui.calendar;
+
+import androidx.constraintlayout.motion.widget.MotionLayout;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+public class CalendarViewModel {
+    private ConstraintLayout.
+    public CalendarViewModel(){
+
+    }
+}
