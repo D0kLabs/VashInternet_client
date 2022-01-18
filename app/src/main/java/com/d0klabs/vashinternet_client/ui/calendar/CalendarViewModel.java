56593d7aaf2dfbfedb1ceb7a12654f6781567b89
D0k <devtemp411@bigmir.net>
2022-01-18 17:04:41 +0200

    calendar building

--- app/src/main/java/com/d0klabs/vashinternet_client/ui/calendar/CalendarViewModel.java
+++ app/src/main/java/com/d0klabs/vashinternet_client/ui/calendar/CalendarViewModel.java
@@ -1,11 +1,27 @@
 package com.d0klabs.vashinternet_client.ui.calendar;
 
+import android.view.MotionEvent;
+
 import androidx.constraintlayout.motion.widget.MotionLayout;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.core.view.MotionEventCompat;
 
 public class CalendarViewModel {
-    private ConstraintLayout.
+    private MotionLayout motionLayout;
+    private MotionEventCompat motionEventCompat;
+    private MotionEvent mMotionEventAll;
+
+    public void draw(){
+
+    };
+
+    public int set() {
+        return 0;
+    }
+
     public CalendarViewModel(){
+
+
 
     }
 }
